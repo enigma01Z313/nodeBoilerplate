c9742720eecedc1c2bfd7c323de0bd4d208c8475
enigma01Z313 <f.ahmadyf94@gmail.com>
2022-12-26 00:29:23 +0330

    ww

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.STAGING ? 30004 : process.env.PORT ?? 30003;
 
 const server = http.createServer(app);
 
-/////////////////
+// const host = "http://67.43.234.92/"
 const host = "http://loclhost";
 server.listen(port, () => {
   console.log(`Api server is running on: ${host}:${port}`);
