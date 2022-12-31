9aaad914fa7bd6fbf5ac9bb406350f6f9cffc4e1
enigma01Z313 <f.ahmadyf94@gmail.com>
2022-12-31 14:40:35 +0330

    updated assosiates file

--- app/api/db/MySQL/assosiates.js
+++ app/api/db/MySQL/assosiates.js
@@ -1,7 +1,11 @@
 module.exports = (db) => {
-  const { User, Role } = db;
+  const { User, Role, UserMeta } = db;
 
   User.belongsTo(Role, { foreignKey: "role_id" });
+
+  User.hasMany(UserMeta, { foreignKey: "userId" });
+
+  UserMeta.belongsTo(User, { foreignKey: "userId" });
 
   return db;
 };
