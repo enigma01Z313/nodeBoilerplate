7b29597cb4a034c7e0a2fd7b18ce003b2856fe7c
enigma01Z313 <f.ahmadyf94@gmail.com>
2023-01-02 09:55:35 +0330

    book ID

--- app/api/db/MySQL/models/book.js
+++ app/api/db/MySQL/models/book.js
@@ -2,6 +2,11 @@
 
 module.exports = (sequelize, DataTypes) =>
   sequelize.define("book", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     uuid: {
       allowNull: false,
       type: DataTypes.UUID,
