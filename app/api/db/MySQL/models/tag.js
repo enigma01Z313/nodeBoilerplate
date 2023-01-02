78c00e9128d59d71591279d10513abe9ec8c8422
enigma01Z313 <f.ahmadyf94@gmail.com>
2023-01-02 10:32:26 +0330

    create Tag model

---
+++ app/api/db/MySQL/models/tag.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define("tag", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    uuid: {
+      allowNull: false,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  });
