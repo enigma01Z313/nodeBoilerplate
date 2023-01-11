e37759151f1b9ab2347dcf1f9fa065ae82c6a7a0
Ahmadi Farzin <F.Ahmadi@Rayan.LAN>
2023-01-11 11:04:26 +0330

    merge confilict

--- app/api/db/MySQL/scripts/reset.js
+++ app/api/db/MySQL/scripts/reset.js
@@ -4,14 +4,10 @@ const db = require("../models");
 
 db.sequelize
   .query("SET FOREIGN_KEY_CHECKS = 0")
-<<<<<<< HEAD
-  .sync({ force: true })
-  .query("SET FOREIGN_KEY_CHECKS = 1")
   .then(() => {
     console.log("Database has been reset and ready to use");
     process.exit;
-  });
-=======
+  })
   .then(function () {
     return db.sequelize.sync({ force: true });
   })
@@ -26,4 +22,3 @@ db.sequelize
       console.log(err);
     }
   );
->>>>>>> bookCategory
