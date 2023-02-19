b6b3fdba39fb05abc55c500ec82d4b7aceeea661
enigma01Z313 <f.ahmadyf94@gmail.com>
2023-02-19 21:08:21 +0330

    updated wallet route

---
+++ app/api/routes/wallets.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+const use = require("../src/utils/use");
+const {
+  serveJson,
+  Auth: { authentication, authorization },
+  Validate: {
+    index: { ValidateF, validator },
+  },
+  getDataList,
+  filteredData,
+  filteredSearch,
+} = require("../src/middleware");
+
+const {
+  Wallet: { list, get },
+} = require("../src/services");
+
+/**************************/
+/*         routes         */
+/**************************/
+router.get(
+  "/",
+  use(authentication),
+  use(filteredData({})),
+  use(
+    filteredSearch({
+      model: "Wallet",
+      fields: ["ownerId", "amount"],
+    })
+  ),
+  use(getDataList("Wallet", "کیف پول", undefined, undefined, "walletList")),
+  serveJson
+);
+
+router.get("/:uuid", use(authentication), use(get), serveJson);
+
+module.exports = router;
