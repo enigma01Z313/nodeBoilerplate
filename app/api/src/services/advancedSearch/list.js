0c0e1a4fcc4e5da2106bd5df05c16d09bd633045
Ahmadi Farzin <F.Ahmadi@Rayan.LAN>
2023-02-21 17:38:41 +0330

    ss

--- app/api/src/services/advancedSearch/list.js
+++ app/api/src/services/advancedSearch/list.js
@@ -1,25 +1,28 @@
+const { Op } = require("sequelize");
 const { Author, User, Off_price, Book } = require("../../../db/MySQL/models");
 
 const { inspect } = require("../../utils");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   const criteria = req.query.criteria ?? "book";
   const bookType = req.query.bookType ?? "book";
 
   const include = [{ model: Off_price }];
+  const where = {};
 
   if (criteria === "all") {
     include.push(
       { model: Author, as: "authors" },
       { model: User, as: "publisher", where: { roleId: 4 } }
     );
+    where[Op.or] = [];
   } else {
   }
-  let defaultOptions = { include };
+  let defaultOptions = { include, where };
 
   inspect(defaultOptions);
 
-  const books = Book.findAll();
+  const books = await Book.findAll(defaultOptions);
 
   return res.json(books);
 };
