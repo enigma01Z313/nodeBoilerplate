ad0f2fc68b8a0f54f32868e401d23505d047cdb7
enigma01Z313 <f.ahmadyf94@gmail.com>
2023-03-05 10:13:27 +0330

    updated swagger / added cards to publisher

--- app/api/routes/publishers.js
+++ app/api/routes/publishers.js
@@ -20,10 +20,13 @@ const {
 const {
   Publisher: {
     get,
-    // Book: { list: bookList },
+    Card: { list: listCards },
   },
 } = require("../src/services");
 
+const services = require("../src/services");
+
+console.log(services);
 /**************************/
 /*         routes         */
 /**************************/
@@ -41,6 +44,7 @@ const publisherBooksOption = {
   baseModel: "publisher",
   includes: [{ model: "Off_price" }],
 };
+
 router.get(
   "/:uuid/books",
   use(bookQuery),
@@ -53,6 +57,16 @@ router.get(
     })
   ),
   use(bookList(publisherBooksOption)),
+  serveJson
+);
+
+router.get(
+  "/:uuid/cards",
+  use(authentication),
+  use(filteredData({})),
+  use(getEntityByUuid({ model: "User", fields: ["uuid"] })),
+  use(sortedData),
+  use(listCards),
   serveJson
 );
 
